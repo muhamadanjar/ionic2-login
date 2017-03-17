d81fad296e72a98db96127f16643798014011014
Muhamad Anjar <arvanria@gmail.com>
2017-03-17 16:55:51 +0700

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { IonicApp, IonicModule } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { Storage } from '@ionic/storage';
+import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login-page/login-page';
+import { SignupPage } from '../pages/signup-page/signup-page';
+import { Todos } from '../providers/todos';
+import { Auth } from '../providers/auth';
+
+
+
+export function provideStorage() {
+ return new Storage();
+}
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    SignupPage
+  ],
+  imports: [
+    IonicModule.forRoot(MyApp),
+    
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    LoginPage,
+    SignupPage
+  ],
+  providers: [Todos, Auth,{ provide: Storage, useFactory: provideStorage }]
+})
+export class AppModule {}
